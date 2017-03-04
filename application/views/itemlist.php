679fce4e178e9824859d4677086839fc0c99775a
mrosenlund <mrosenlund@my.bcit.ca>
2017-03-04 15:55:50 -0800

    Job 6 completed - Pagination

--- application/views/itemlist.php
+++ application/views/itemlist.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-	<table class="table">
+{pagination}
+<table class="table">
         <tr>
 			<th>Id</th>
 			<th>Task</th>
 			<th>Status</th>
         </tr>
         {display_tasks}
-	</table>
-</html>
+</table>
